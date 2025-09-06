bc9d66c676d1987c41265ff352d91aacd720b22b
Rohit <r.dhakad30@gmail.com>
2025-09-06 11:17:04 +0530

    Initial Commit
    Initial Commit

---
+++ MovingClub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovingClub
+//
+//  Created by Rohit SIngh Dhakad on 06/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
